593a5580f026736ad92690ab0073a068ac5086bc
Rafael Teixeira <37218706+Rafaelrato1@users.noreply.github.com>
2020-04-23 19:49:45 -0300

    Add files via upload
    Exercício sobre POO em JavaScript para o treinamento da Visão.

---
+++ ex1.js
@@ -0,0 +1,68 @@
+var ana = {
+  nome: 'Ana',
+  idade: 17,
+  isGarcom: false,
+  uni: universidade,
+  rep: {},
+  irmao: pedro
+}
+
+var pedro = {
+  nome: 'Pedro',
+  idade: 20,
+  isGarcom: false,
+  uni: universidade,
+  rep: undefined,
+  irmao: ana
+}
+
+var paulo = {
+  nome: 'Paulo',
+  idade: 23,
+  isGarcom: false,
+  uni: undefined,
+  rep: republica,
+  irmao: undefined
+}
+
+var armando = {
+  nome: 'Armando',
+  idade: 17,
+  isGarcom: false,
+  uni: undefined,
+  rep: republica,
+  irmao: undefined
+}
+
+var andre = {
+  nome: 'André',
+  idade: undefined,
+  isGarcom: true,
+  uni: undefined,
+  rep: undefined,
+  irmao: undefined
+}
+
+var universidade = {
+  nome: 'Universidade Federal de Ouro Preto',
+  curso: 'Engenharia da Computação',
+  cargaHoraria: 3630
+}
+
+var republica = {
+  nome: 'República Orfanato',
+  idade: 10,
+  qtdMoradores: 5
+}
+
+function checarIDs (garcom, cliente){
+  if( garcom.isGarcom && cliente.idade >= 18 ){
+    return cliente.nome + ' pode tomar cerveja'
+  }else{
+    return cliente.nome + ' vai tomar Coca-Cola'
+  }
+
+}
+
+console.log(checarIDs(andre,pedro));
+console.log(checarIDs(andre, armando));
